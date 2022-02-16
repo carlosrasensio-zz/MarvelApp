57aa2f4260efad964939882dec1d160596988ff8
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2022-02-16 14:54:46 +0100

    Added new files to skeleton.

---
+++ MarvelApp/MarvelApp/Detail/Router/SuperheroDetailRouter.swift
@@ -0,0 +1,8 @@
+//
+//  SuperheroDetailRouter.swift
+//  MarvelApp
+//
+//  Created by crodrigueza on 16/2/22.
+//
+
+import Foundation
---
+++ MarvelApp/MarvelApp/Home/Router/SuperheroListRouter.swift
@@ -0,0 +1,8 @@
+//
+//  SuperheroListRouter.swift
+//  MarvelApp
+//
+//  Created by crodrigueza on 16/2/22.
+//
+
+import Foundation
