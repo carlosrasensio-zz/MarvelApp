a38926c127876c5a387afa40acd4b9a8942bba75
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2022-02-16 23:43:31 +0100

    Created home table view.

--- MarvelApp/MarvelApp/Home/View/SuperheroListViewController.swift
+++ MarvelApp/MarvelApp/Home/View/SuperheroListViewController.swift
@@ -8,22 +8,66 @@
 import UIKit
 
 class SuperheroListViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Table view configuration
+    func setTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+
+    // MARK: - Activity indicator configuraion
+    func setActivityIndicator() {
+
     }
 
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - TableView functions
+extension SuperheroListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.CustomCells.movieCellId) as! MovieCustomCell
+        if searchController.isActive && searchController.searchBar.text != "" {
+            cell.titleLabel.text = filteredMovies[indexPath.row].title
+            cell.descriptionLabel.text = filteredMovies[indexPath.row].description
+            cell.voteAverageLabel.text = "Vote average: \(filteredMovies[indexPath.row].voteAverage)"
+            let imageUrl = Constants.NetworkManager.URLs.image + filteredMovies[indexPath.row].image
+            cell.movieImageView.getImageFromURL(urlString: imageUrl)
+        } else {
+            cell.titleLabel.text = movies[indexPath.row].title
+            cell.descriptionLabel.text = movies[indexPath.row].description
+            cell.voteAverageLabel.text = "Vote average: \(movies[indexPath.row].voteAverage)"
+            let imageUrl = Constants.NetworkManager.URLs.image + movies[indexPath.row].image
+            cell.movieImageView.getImageFromURL(urlString: imageUrl)
+        }
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+}
+
+extension SuperheroListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            viewModel.createMovieDetailView(movie: filteredMovies[indexPath.row])
+        } else {
+            viewModel.createMovieDetailView(movie: movies[indexPath.row])
+        }
+    }
 }
