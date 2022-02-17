6d33d7cb6c93c7ee52552fdf0c2cc672ad453f9e
Carlos Rodriguez Asensio <crodrigueza@vectoritcgroup.com>
2022-02-17 01:25:40 +0100

    Created Constants file for app utils and networking URL construction.

--- MarvelApp/MarvelApp/Utils/Constants.swift
+++ MarvelApp/MarvelApp/Utils/Constants.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Constants {
+    static let appName = "Marvel App"
+    static let appIcon = "icnMarvel"
+
+    struct NetworkManager {
+        static let version = 1
+        static let type = "public"
+        static let timeStamp = Date().timeIntervalSince1970
+        static let publicApiKey = "bdd7f1b10e8f1b342807f0d5a01f3e03"
+        static let privateApiKey = ""   // TODO: Insert private api key
+
+        struct URLs {
+            static let base = "https://gateway.marvel.com/"
+        }
+
+        struct Endpoints {
+            static let version = "v\(NetworkManager.version)/"
+            static let type = "\(NetworkManager.type)/"
+            static let timeStamp = "?ts=\(NetworkManager.timeStamp)"
+            static let hash = "&hash="
+            static let apiKey = "?api_key=\(Constants.NetworkManager.publicApiKey)"
+            static let characters = "characters"
+        }
+    }
+
+    struct CustomCells {
+        static let superheroCellId = "SuperheroCustomCell"
+    }
+}
